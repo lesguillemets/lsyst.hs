fc7f689dc01bcea81c81a6d6a0ae51f2078cad49
lesguillemets <lesguillemets@users.noreply.github.com>
2015-08-31 14:16:34 +0900

    Pythagoras

--- src/LSys/PythagorasTree.hs
+++ src/LSys/PythagorasTree.hs
@@ -1,16 +1,40 @@
 module LSys.PythagorasTree (pythagoras) where
 import LSys.LSys
+import Turtle
 import qualified Data.Set as S
+import Graphics.Gloss
 
 pythagoras :: LSys Char
 pythagoras = LSys {
     _alph = S.fromList "01[]",
     _state = "0",
     _rule = pyrule,
-    _display = undefined
+    _display = pyDraw
 }
 
 pyrule :: Char -> String
 pyrule '1' = "11"
 pyrule '0' = "1[0]0"
 pyrule c = return c
+
+pyDraw :: [Char] -> IO()
+pyDraw = turtleSimlate "Pyt" (Turtle (0,0) 0) [] . concatMap toCommands
+
+-- TODO: Better implementation
+toCommands :: Char -> [Turtle -> Stack -> (Command, Stack)]
+toCommands '0' = return $ \ _ s -> (Draw leafLength, s)
+toCommands '1' = return $ \ _ s -> (Draw baseLength, s)
+toCommands '[' = return $ \t s -> (Turn (4/pi), _loc t:s)
+toCommands ']' = [
+                 \_ s -> (Turn (-4/pi), s),
+                 \t s -> (JumpTo (head s), tail s)
+                 ]
+-- toCommands '1' = \ _ s return $ (Draw baseLength, s))
+-- toCommands '[' = \ t s return $ (Turn (4/pi), (_loc t:s)))
+-- toCommands ']' = \ t s [(Turn (-4/pi), s), (JumpTo (head s), tail s)])
+
+type Stack = [(Double,Double)]
+leafLength :: Double
+leafLength = 10
+baseLength :: Double
+baseLength = 20
--- src/Turtle.hs
+++ src/Turtle.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE RecordWildCards #-}
-module Turtle (glossDraw, Turtle) where
+{-# LANGUAGE BangPatterns #-}
+module Turtle (turtleDraw, turtleSimlate, Turtle(..), Command(..)) where
 import Control.Arrow
 import GHC.Float (double2Float)
 import Graphics.Gloss
@@ -8,9 +9,16 @@ import Graphics.Gloss
 window :: String -> Display
 window s = InWindow s (500,500) (30,30)
 
-glossDraw :: String -> Turtle -> [Command] -> IO ()
-glossDraw s it cmds = display (window s) black $
+turtleDraw :: String -> Turtle -> [Command] -> IO ()
+turtleDraw s it cmds = display (window s) black $
     Pictures (execCommands it cmds)
+
+turtleSimlate :: String -- title
+              -> Turtle -> s -- initial turtle and initial state
+              -> [Turtle -> s -> (Command, s)] -- commands
+              -> IO ()
+turtleSimlate title initTurtle initState cmds = display (window title) black $
+    Pictures (simTurtle initTurtle initState cmds)
 
 data Turtle = Turtle {
             _loc :: (Double, Double),
@@ -27,14 +35,25 @@ floatise (x,y) = (double2Float x, double2Float y)
 data Command = Jump Double
              | Draw Double
              | Turn Double
+             | JumpTo (Double, Double)
+             | DrawTo (Double, Double)
 
+-- TODO :: Better abstraction
 execCommands :: Turtle -> [Command] -> [Picture]
-execCommands t [] = []
-execCommands t@(Turtle{..}) (c:cs) =
-        case c of
-            (Jump d) -> execCommands (t{_loc = _loc +: forward t d}) cs
-            (Draw d) -> let
-                newLoc = _loc +: forward t d in
-                    (color white . line . map floatise $ [_loc, newLoc]):
-                        execCommands (t{_loc = newLoc}) cs
-            (Turn arg) -> execCommands (t{_dir = _dir + arg}) cs
+execCommands initTurtle = simTurtle initTurtle undefined . map (
+        \c -> (\_ _ -> (c, undefined)))
+
+-- TODO : Rewrite using State Monad
+simTurtle :: Turtle -> s -> [Turtle -> s -> (Command, s)] -> [Picture]
+simTurtle _ _ [] = []
+simTurtle !t@(Turtle{..}) s (cFunc:cs) = let (c, nextState) = cFunc t s in
+    case c of
+        (Jump d) -> simTurtle (t{_loc = _loc +: forward t d}) nextState cs
+        (Draw d) -> let
+            newLoc = _loc +: forward t d in
+                (color white . line . map floatise $ [_loc, newLoc]):
+                    simTurtle (t{_loc = newLoc}) nextState cs
+        (Turn arg) -> simTurtle (t{_dir = _dir + arg}) nextState cs
+        (JumpTo d) -> simTurtle (t{_loc = d}) nextState cs
+        (DrawTo d) -> (color white . line . map floatise $ [_loc,d]):
+                            simTurtle (t{_loc=d}) nextState cs
